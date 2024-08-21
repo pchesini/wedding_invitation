16748fd58815cfdccaef642d080086af05a70caf
Pablo Chesini <pchesini1@gmail.com>
2024-08-21 17:34:13 -0300

    Agrego fotos para probar el carrousel

--- src/app/photo-list/photo-list.component.ts
+++ src/app/photo-list/photo-list.component.ts
@@ -7,9 +7,15 @@ import { Component } from '@angular/core';
 })
 export class PhotoListComponent {
   photos = [
+    { src: 'assets/fotos/PICT0048.jpg', alt: 'Descripción de la foto 1' },
+    { src: 'assets/fotos/PICT0052.jpg', alt: 'Descripción de la foto 2' },
+    { src: 'assets/images/photo3.jpg', alt: 'Descripción de la foto 3' }
+  ]
+  
+  /* photos = [
     { url: 'assets/photo1.jpg', description: 'Descripción de la foto 1' },
     { url: 'assets/photo2.jpg', description: 'Descripción de la foto 2' },
     { url: 'assets/photo3.jpg', description: 'Descripción de la foto 3' },
-    // Agrega más fotos aquí
-  ];
+    
+  ]; */
 }
